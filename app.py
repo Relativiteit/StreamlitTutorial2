e7a497e9645d90dd1ba9b4ea32e865e258ab9381
Relativiteit <alejocain@gmail.com>
2023-08-31 11:33:45 +0200

    selection dropdown

--- app.py
+++ app.py
@@ -7,19 +7,14 @@ import streamlit as st
 import numpy as np 
 import pandas as pd 
 
-x: int = st.slider('x')
+df = pd.DataFrame({ 
+    'first column': [1,2,3,4],
+    'second column': [10, 20, 30, 40]
+})
 
-st.text_input("Your name", key ="name")
+option = st.selectbox(
+    'Which number do you like best?',
+    df['second column'],
+)
 
-
-st.write(x, 'squared is', x * x )
-
-if st.checkbox("show data for map"):
-    map_data = pd.DataFrame(
-    np.random.rand(1000,2) / [50, 50] + [37.76 - 122.4],
-    columns=['lat', 'lon'])
-
-    map_data
-    
-# st.map(map_data)
-
+'You selected: ', option 
