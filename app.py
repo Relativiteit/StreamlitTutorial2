72c1e8f9fb859c321005e356de2bd35420b91497
Relativiteit <alejocain@gmail.com>
2023-08-31 11:11:22 +0200

    max highlight

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import streamlit as st
 import numpy as np 
 import pandas as pd 
 
-dataframe = np.random.rand(10,20)
-st.dataframe(dataframe)
+dataframe = pd.DataFrame(
+    np.random.rand(10,20),
+    columns=('col %d' % i for i in range(20))
+)
 
+st.dataframe(dataframe.style.highlight_max(axis=0))
